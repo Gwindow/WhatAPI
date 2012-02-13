2fafd2034094c21027d12c708bea03ebdae53335
Gwindow <tim.pirate@gmail.com>
2012-02-12 23:44:30 -0800

    stuff

--- src/api/cli/Tester.java
+++ src/api/cli/Tester.java
@@ -1,7 +1,10 @@
 package api.cli;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import api.inbox.conversation.Conversation;
 import api.soup.MySoup;
-import api.subscriptions.Subscriptions;
 import api.util.CouldNotLoadException;
 
 /**
@@ -24,10 +27,13 @@ public class Tester {
 		 * rs.getResponse().getResults().size(); i++) {
 		 * System.out.println(rs.getResponse().getResults().get(i).getTitle()); }
 		 */
-		Subscriptions s = Subscriptions.init();
-		for (int i = 0; i < s.getResponse().getThreads().size(); i++) {
-			System.out.println(s.getResponse().getThreads().get(i).getThreadTitle());
-		}
+		// Subscriptions s = Subscriptions.init();
+		// for (int i = 0; i < s.getResponse().getThreads().size(); i++) {
+		// System.out.println(s.getResponse().getThreads().get(i).getThreadTitle());
+		// }
+		Conversation c = Conversation.conversationFromId(3202632);
+		System.err.println(split(c.getResponse().getMessages().get(0).getBody(), "alt=", "src"));
+		System.out.println((c.getResponse().getMessages().get(0).getBody()));
 
 	}
 
@@ -41,6 +47,15 @@ public class Tester {
 	 */
 	public static void main(String[] args) throws CouldNotLoadException {
 		new Tester();
+
+	}
+
+	public String split(String string, String from, String to) {
+		Pattern pattern = Pattern.compile(".*" + from + "(.*)" + to + ".*");
+		Matcher matcher = pattern.matcher(string);
+		if (matcher.matches())
+			return matcher.group(1);
+		return null;
 	}
 
 }
