0ff2f02fea6a854dd59f138ac90d1d82fc5ebfdb
Will <willusher.life@gmail.com>
2014-03-29 21:33:17 -0700

    Add ability to vote on a request

--- src/api/requests/Request.java
+++ src/api/requests/Request.java
@@ -24,9 +24,18 @@ public class Request {
 	 * @return the request
 	 */
 	public static Request fromId(int id) {
-		String authkey = MySoup.getAuthKey();
-		String url = "ajax.php?action=request&id=" + id + "&auth=" + authkey;
+		String url = "ajax.php?action=request&id=" + id + "&auth=" + MySoup.getAuthKey();
 		return (Request) MySon.toObject(url, Request.class);
+	}
+
+	/**
+	 * Add some bounty to the request
+	 * @param amount bounty to add in bytes
+	 */
+	public void vote(long amount){
+		String url = "requests.php?action=takevote&id=" + response.getRequestId()
+			+ "&auth=" + MySoup.getAuthKey() + "&amount=" + Long.toString(amount);
+		MySoup.pressLink(url);
 	}
 
 	/**
