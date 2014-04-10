ccec9ef3a2a678d69ab26024aecbb87149ccf2c4
Will <willusher.life@gmail.com>
2014-04-09 21:27:42 -0700

    More useful data to be stored in the barcode such as tags from semantics and an optional user label
    Also re-order the data from MusicBrainzParser to be Artist Album instead of the other way around

--- src/api/search/crossreference/MusicBrainzParser.java
+++ src/api/search/crossreference/MusicBrainzParser.java
@@ -8,34 +8,52 @@ import org.xml.sax.helpers.DefaultHandler;
  * MusicBrainzParser is a simple xml SAX Handler for MusicBrainz Release Queries with &limit=1
  */
 public class MusicBrainzParser extends DefaultHandler {
-    boolean name = false;
-    boolean artist = false;
-    boolean title = false;
+	boolean name = false;
+	boolean artist = false;
+	boolean title = false;
 
-    public String searchString = "";
+	public String searchString = "";
 
-    public void startElement(String namespaceURI, String localName, String qName, Attributes atts) throws SAXException {
-        if(qName.equalsIgnoreCase("artist")) {
-            artist = true;
-        } else if(qName.equalsIgnoreCase("name")){
-            name = true;
-        } else if(qName.equalsIgnoreCase("title")) {
-            title = true;
-        }
-    }
-    public void characters(char ch[], int start, int length) throws SAXException {
-        if((artist && name) || title){
-            searchString += new String(ch, start, length);
-	        searchString += " ";
-        }
-    }
-    public void endElement(String uri, String localName, String qName)throws SAXException {
-        if(qName.equalsIgnoreCase("name")) {
-            name = false;
-        } else if(qName.equalsIgnoreCase("title")) {
-            title = false;
-        } else if(qName.equalsIgnoreCase("artist")){
-            artist = false;
-        }
-    }
+	@Override
+	public void startElement(String namespaceURI, String localName, String qName, Attributes atts) throws SAXException{
+		if (qName.equalsIgnoreCase("artist")){
+			artist = true;
+		}
+		else if (qName.equalsIgnoreCase("name")){
+			name = true;
+		}
+		else if (qName.equalsIgnoreCase("title")){
+			title = true;
+		}
+	}
+
+	@Override
+	public void characters(char ch[], int start, int length) throws SAXException{
+		//We want artist name to appear before the title
+		if (artist && name){
+			if (!searchString.isEmpty()){
+				searchString = " " + searchString;
+			}
+			searchString = new String(ch, start, length) + searchString;
+		}
+		else if (title){
+			if (!searchString.isEmpty()){
+				searchString += " ";
+			}
+			searchString += new String(ch, start, length);
+		}
+	}
+
+	@Override
+	public void endElement(String uri, String localName, String qName) throws SAXException{
+		if (qName.equalsIgnoreCase("name")){
+			name = false;
+		}
+		else if (qName.equalsIgnoreCase("title")){
+			title = false;
+		}
+		else if (qName.equalsIgnoreCase("artist")){
+			artist = false;
+		}
+	}
 }
